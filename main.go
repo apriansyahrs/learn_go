52f2013527b2d8480588d79be372ab58d206590e
apriansyahrs <apriansyahrizs@gmail.com>
2024-02-26 19:59:59 +0700

    create your first project with go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// func main () {
+// 	fmt.Println("Ariell Jordan")
+// 	fmt.Println("Ariell", "Jordan")
+
+// 	fmt.Print("Ariell Jordan\n")
+// 	fmt.Print("Ariell", " Jordan\n")
+// 	fmt.Print("Ariell", " ", " Jordan\n")
+// }
+
+
+// func main () {
+// 	// Komentar kode
+// 	// Menghasilkan pesan hello world
+
+// 	fmt.Println("Hello World")
+
+// 	// fmt.Println("Baris ini tidak akan dieksekusi")
+// }
+
+
+func main () {
+	/*
+	Komentar kode
+	Menghasilkan pesan hello world
+	*/
+
+	fmt.Println("Hello World")
+
+	// fmt.Println("Baris ini tidak akan dieksekusi")
+}
